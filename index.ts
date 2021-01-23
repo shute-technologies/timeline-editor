ee11f01f5a58842f5902f40655cfaa98ba67c3e3
Cristian F. Shute <cristian.flores@clariti.net>
2021-01-22 21:24:49 -0500

    exposing classes

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export * from './src/shute-technologies/modules/runtime/ateEnumDataType';
 export * from './src/shute-technologies/modules/runtime/ateEnumTweenType';
 export * from './src/shute-technologies/modules/runtime/atePlaybackEngine';
 export * from './src/shute-technologies/modules/common/ateInterfaces';
+export * from './src/shute-technologies/modules/editor/ateEngine';
+export * from './src/shute-technologies/modules/components/ateHtmlButton';
