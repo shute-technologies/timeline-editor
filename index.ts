46ca7e9b8002cf3db9f1c2b0a6174dfc97a9220a
Cristian F. Shute <cristian.flores@clariti.net>
2021-01-25 17:02:01 -0500

    exposing classes

--- index.ts
+++ index.ts
@@ -5,3 +5,4 @@ export * from './src/shute-technologies/modules/common/ateInterfaces';
 export * from './src/shute-technologies/modules/editor/ateEngine';
 export * from './src/shute-technologies/modules/components/ateHtmlButton';
 export * from './src/shute-technologies/modules/config/ateStyles';
+export * from './src/shute-technologies/modules/editor/helpers/ateEngineHelper';
